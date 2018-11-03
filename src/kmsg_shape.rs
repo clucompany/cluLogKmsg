44566773fc7129c63e76bbbab4024792fce8272f
Денис Котляров <denis2005991@gmail.com>
2018-11-04 00:04:36 +0300

    1. Add kmsg out.

--- src/kmsg_shape.rs
+++ src/kmsg_shape.rs
@@ -18,48 +18,48 @@ impl LogShape for KmsgShape {
 	
 	#[inline(always)]
 	fn warning<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [WAR] {}\n", crate_name!(), display)	)
+		write.write_fmt(	format_args!("<4>{}: [WAR] {}\n", crate_name!(), display)	)
 	}
 	//[WAR] - warning value
 	
 	#[inline(always)]
 	fn info<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [INF] {}\n", crate_name!(), display)	)
+		write.write_fmt(	format_args!("<5>{}: [INF] {}\n", crate_name!(), display)	)
 	}
 	//[INF] - info value
 	
 	#[inline(always)]
 	fn error<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [ERR] {}\n", crate_name!(), display)		)
+		write.write_fmt(	format_args!("<3>{}: [ERR] {}\n", crate_name!(), display)		)
 	}
 	//[ERR] - err value
 	
 	#[inline(always)]
 	fn panic<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [PANIC] {}\n", crate_name!(), display)	)
+		write.write_fmt(	format_args!("<3>{}: [PANIC] {}\n", crate_name!(), display)	)
 	}
 	//[PANIC] - panic program
 	
 	#[inline(always)]
 	fn unknown<'a, W: Write>(mut write: W, name: &'static str, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [{}] {}\n", crate_name!(), name, display)	)
+		write.write_fmt(	format_args!("<6>{}: [{}] {}\n", crate_name!(), name, display)	)
 	}
 	//[UNK] - unknown 
 	
 	#[inline(always)]
 	fn trace<'s, W: Write>(mut write: W, line: u32, pos: u32, file: &'static str, args: Arguments<'s>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [TRACE][{}][{}:{}] {}\n", crate_name!(), file, line, pos, args)	)
+		write.write_fmt(	format_args!("<6>{}: [TRACE][{}:{}:{}] {}\n", crate_name!(), file, line, pos, args)	)
 	}
 	
 	#[inline(always)]
 	fn print<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [OUT] {}", crate_name!(), display)		)
+		write.write_fmt(	format_args!("<5>{}: [OUT] {}", crate_name!(), display)		)
 	}
 	//[OUT] - unknown 
 	
 	#[inline(always)]
 	fn eprint<'a, W: Write>(mut write: W, display: Arguments<'a>) -> io::Result<()> {
-		write.write_fmt(	format_args!("{}: [EOUT] {}", crate_name!(), display)		)
+		write.write_fmt(	format_args!("<3>{}: [EOUT] {}", crate_name!(), display)		)
 	}
 	//[EOUT] - unknown 
 }
